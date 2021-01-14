b7285be57f0aed0b31a382f647f60d08016dd26b
Sergey Fedorov <sergeyfedorov2@gmail.com>
2021-01-14 14:21:38 +0300

    Moved idt

--- lab2/kernel/kernel.c
+++ lab2/kernel/kernel.c
@@ -1,77 +1,10 @@
 #include "../drivers/keyboard_map.h"
 #include "../drivers/screen.h"
-
-#define PIC_1_CTRL 0x20
-#define PIC_2_CTRL 0xA0
-#define PIC_1_DATA 0x21
-#define PIC_2_DATA 0xA1
-#define IDT_SIZE 256
-
-struct idt_pointer
-{
-    unsigned short limit;
-    unsigned int base;
-} __attribute__((packed));
-
-struct idt_entry
-{
-    unsigned short int offset_lowerbits;
-    unsigned short int selector;
-    unsigned char zero;
-    unsigned char flags;
-    unsigned short int offset_higherbits;
-} __attribute__((packed));
-
-struct idt_entry idt_table[IDT_SIZE];
-struct idt_pointer idt_ptr;
-
-static void initialize_idt_pointer()
-{
-    idt_ptr.limit = (sizeof(struct idt_entry) * IDT_SIZE) - 1;
-    idt_ptr.base = (unsigned int)idt_table;
-}
+#include "../drivers/idt.h"
 
 extern unsigned char read_port(int port);
 extern void write_port(unsigned short port, unsigned char data);
-extern void load_idt(void*);
 extern void keyboard_handler_int();
-
-void load_idt_entry(int isr_number, unsigned long base, short int selector, unsigned char flags)
-{
-    idt_table[isr_number].offset_lowerbits = base & 0xFFFF;
-    idt_table[isr_number].offset_higherbits = (base >> 16) & 0xFFFF;
-    idt_table[isr_number].selector = selector;
-    idt_table[isr_number].flags = flags;
-    idt_table[isr_number].zero = 0;
-}
-
-static void initialize_pic()
-{
-    /* ICW1 - begin initialization */
-    write_port(PIC_1_CTRL, 0x11);
-    write_port(PIC_2_CTRL, 0x11);
-
-    /* ICW2 - remap offset address of idt_table */
-    /*
-    * In x86 protected mode, we have to remap the PICs beyond 0x20 because
-    * Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
-    */
-    write_port(PIC_1_DATA, 0x20);
-    write_port(PIC_2_DATA, 0x28);
-
-    /* ICW3 - setup cascading */
-    write_port(PIC_1_DATA, 0x00);
-    write_port(PIC_2_DATA, 0x00);
-
-    /* ICW4 - environment info */
-    write_port(PIC_1_DATA, 0x01);
-    write_port(PIC_2_DATA, 0x01);
-    /* Initialization finished */
-
-    /* mask interrupts */
-    write_port(0x21 , 0xff);
-    write_port(0xA1 , 0xff);
-}
 
 void keyboard_handler(void)
 {
@@ -110,13 +43,6 @@ void kb_init(void)
     /* 0xFD is 11111101 - enables only IRQ1 (keyboard) on master pic
        by clearing bit 1. bit is clear for enabled and bit is set for disabled */
     write_port(0x21, curmask_master & 0xFD);
-}
-
-void idt_init()
-{
-    initialize_pic();
-    initialize_idt_pointer();
-    load_idt(&idt_ptr);
 }
 
 void kmain() {
